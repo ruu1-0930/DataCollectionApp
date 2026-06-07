95968f4a364749f39a228c76e793475d206564e5
Ruu1 <yr18861636090@gmail.com>
2026-06-07 13:40:56 +0800

    feat: /clinician/me 读/改医护资料

--- back/tests/test_clinician.py
+++ back/tests/test_clinician.py
@@ -32,3 +32,26 @@ def test_login_wrong_passcode_rejected(client):
     resp = client.post('/clinician/login', json={
         'phone': '13800000000', 'terminal_code': 'term-1', 'passcode': '0000'})
     assert resp.get_json()['code'] != 200
+
+
+def _token(client):
+    resp = client.post('/clinician/enable', json=_enable_payload())
+    return resp.get_json()['data']['token']
+
+
+def test_get_me(client):
+    token = _token(client)
+    resp = client.get('/clinician/me', headers={'Authorization': f'Bearer {token}'})
+    assert resp.get_json()['data']['name'] == '王医生'
+
+
+def test_put_me_updates_dept(client):
+    token = _token(client)
+    resp = client.put('/clinician/me', headers={'Authorization': f'Bearer {token}'},
+                      json={'dept': '康复科'})
+    assert resp.get_json()['data']['dept'] == '康复科'
+
+
+def test_me_requires_token(client):
+    resp = client.get('/clinician/me')
+    assert resp.status_code == 401
